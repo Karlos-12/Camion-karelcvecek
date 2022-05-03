aa1f87a7f3929abb50302f0d8db72a05335279b2
Karel Cveček <Cvecek.Karel@stud.vassboskovice.cz>
2022-05-03 08:44:42 +0200

    Přidejte soubory projektu.

---
+++ Camion/Camion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Camion
+{
+    internal class Camion
+    {
+        public int maxcarry { get;}
+        public int maxfuel { get;}
+        public int consum { get;}
+
+        public int distance { get;}
+
+
+        public int curentcarry { get; set; }
+        public int curentfuel { get; set; }
+
+        public Camion(int nosnost,int velnadr,int spotreba)
+        {
+            maxcarry = nosnost;
+            maxfuel = velnadr;
+            consum = spotreba;
+            distance = 0;
+            curentcarry = 0;
+            curentfuel = 0;
+        }
+
+        public void GO(int distance)
+        {
+            if ((((distance / 100 * consum)/100) * (100 + (curentcarry/(maxcarry/100)))) < curentfuel)
+            {
+                curentfuel = (((distance / 100 * consum) / 100) * (100 + (curentcarry / (maxcarry / 100))));
+            }
+            else
+            {
+                MessageBox.Show("Nemáš dost paliva hhochu!");   
+            }
+        }
+
+
+    }
+}
---
+++ Camion/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Camion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Camion redcamion;
+        Camion bluecamion;
+        public MainWindow()
+        {
+            InitializeComponent();
+            redcamion = new Camion(250, 125, 25);
+            bluecamion = new Camion(275, 100, 35);
+        }
+
+        private void Jet_Click(object sender, RoutedEventArgs e)
+        {
+            int m = 0;
+            try
+            { 
+                m = int.Parse(jetb.Text); 
+            switch(swa)
+                {
+                    case 0:
+                        redcamion.GO(m);
+                        break;
+                    case 1:
+                        bluecamion.GO(m);
+                        break;
+                }         
+            }
+            catch {MessageBox.Show("Napiš tam něco normálního, pls!"); }
+            write();
+        }
+        int swa = 0;
+
+        public void sw_Click(object sender, RoutedEventArgs e)
+        {
+            if(swa == 0)
+            { swa = 1; }
+            else
+            { swa = 0; }
+        }
+
+        public void write()
+        {
+
+        }
+    }
+}
